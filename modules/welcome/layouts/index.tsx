208f25f25330a3fb3d0710355c07a06c38b452c4
wisdom <wisdomwokedev@gmail.com>
2025-08-24 00:18:05 +0100

    chore: added welcome page

--- modules/welcome/layouts/index.tsx
+++ modules/welcome/layouts/index.tsx
@@ -8,25 +8,23 @@ import { WelcomeCard } from "../components/card";
 
 export const WelcomeLayouts = () => {
   return (
-    <div
-        className="px-20 py-10"
-    >
+    <div className="px-4 py-6 sm:px-8 md:px-16 lg:px-20 lg:py-10">
       <GridPatterns />
       <Logo />
       <div className="mt-6">
-        <h1 className="text-3xl font-bold">Hello Wisdom</h1>
-        <p>Your journey to success begins here.</p>
+        <h1 className="text-2xl sm:text-3xl font-bold">Hello Wisdom</h1>
+        <p className="text-base sm:text-lg">Your journey to success begins here.</p>
       </div>
-      <div className="mt-6 flex gap-4">
+      <div className="mt-6 flex flex-col gap-4 md:flex-row">
         <WelcomeCard
-            Icon={Notebook}
-            title="Amidarh Note"
-            description="Notes help you capture ideas, organize your thoughts, and keep track of important information. Start creating notes to boost your productivity and never miss a great idea!"
+          Icon={Notebook}
+          title="Amidarh Note"
+          description="Notes help you capture ideas, organize your thoughts, and keep track of important information. Start creating notes to boost your productivity and never miss a great idea!"
         />
-          <WelcomeCard
-            Icon={BookCheck}
-            title="Amidarh CBT"
-            description="Notes help you capture ideas, organize your thoughts, and keep track of important information. Start creating notes to boost your productivity and never miss a great idea!"
+        <WelcomeCard
+          Icon={BookCheck}
+          title="Amidarh CBT"
+          description="Notes help you capture ideas, organize your thoughts, and keep track of important information. Start creating notes to boost your productivity and never miss a great idea!"
         />
       </div>
     </div>
