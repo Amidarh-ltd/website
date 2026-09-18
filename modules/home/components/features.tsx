cc7c8c223be1fec036749dc034c24635ea49cdf2
WISDOM-WOKE22 <wisdomwokedev@gmail.com>
2026-09-18 11:27:48 +0100

    fix: update image URL in Features component to reflect new live call visual

--- modules/home/components/features.tsx
+++ modules/home/components/features.tsx
@@ -44,7 +44,7 @@ const sections = [
       "Chat & hand-raise",
     ],
     image:
-      "https://res.cloudinary.com/dl5dyntsp/image/upload/v1786843744/cover_2_d8pzgl.png",
+      "https://res.cloudinary.com/dl5dyntsp/image/upload/v1789064274/live_call_tauphy.png",
   },
   {
     number: "03",
